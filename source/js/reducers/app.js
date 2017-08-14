51c1f355d1dce21de0db5e3bb7be2b91275265bb
gspangler <spanglgj@gmail.com>
2017-08-13 21:00:15 -0400

    WT added redux to application

---
+++ source/js/reducers/app.js
@@ -0,0 +1,40 @@
+import { Map } from 'immutable';
+
+import {
+  TEST_ASYNC_ACTION_START,
+  TEST_ASYNC_ACTION_ERROR,
+  TEST_ASYNC_ACTION_SUCCESS,
+} from 'actions/app';
+
+const initialState = Map({
+  asyncLoading: false,
+  asyncError: null,
+  asyncData: null,
+});
+
+const actionsMap = {
+  // Async action
+  [TEST_ASYNC_ACTION_START]: (state) => {
+    return state.merge({
+      asyncLoading: true,
+      asyncError: null,
+    });
+  },
+  [TEST_ASYNC_ACTION_ERROR]: (state, action) => {
+    return state.merge({
+      asyncLoading: false,
+      asyncError: action.data,
+    });
+  },
+  [TEST_ASYNC_ACTION_SUCCESS]: (state, action) => {
+    return state.merge({
+      asyncLoading: false,
+      asyncData: action.data,
+    });
+  },
+};
+
+export default function reducer(state = initialState, action = {}) {
+  const fn = actionsMap[action.type];
+  return fn ? fn(state, action) : state;
+}
